88fb77d586c4fb139c97bec1be16edc5ccea8437
Samba SOW <masssowcode@gmail.com>
2026-01-13 22:58:09 +0100

    ajout reset pswdr iicon

--- src/Form/ResetPasswordRequestForm.php
+++ src/Form/ResetPasswordRequestForm.php
@@ -17,7 +17,7 @@ class ResetPasswordRequestForm extends AbstractType
                 'attr' => ['autocomplete' => 'email'],
                 'constraints' => [
                     new NotBlank([
-                        'message' => 'Please enter your email',
+                        'message' => 'Entrer votre adresse e-mail',
                     ]),
                 ],
             ])
